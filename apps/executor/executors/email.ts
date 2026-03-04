fbd059287e5bc3a01349105cdfa9a9b747e12871
DevDilip28 <dilipasdeo028@gmail.com>
2026-03-05 00:14:43 +0530

    email issue

--- apps/executor/executors/email.ts
+++ apps/executor/executors/email.ts
@@ -19,8 +19,8 @@ export async function sendEmail(to: string, subject: string, body: string) {
   try {
     const response = await resend.emails.send({
       from: "TriggerFlow <no-reply@algoholic.site>",
-      to,
-      subject,
+      to: to,
+      subject: subject,
       html: body,
     });
 
