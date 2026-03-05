dc988897ad6cc414c3d28a6e4a491336dcbf833d
DevDilip28 <dilipasdeo028@gmail.com>
2026-03-05 23:51:03 +0530

    server

--- apps/Server/index.ts
+++ apps/Server/index.ts
@@ -7,24 +7,42 @@ import workflowRoutes from "./routes/workflow.route.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173",
+    "https://trigger-flow-client.vercel.app/"
+  ],
   credentials: true,
 }));
 
-await mongoose.connect(process.env.MONGO_URI!);
+async function startServer() {
+  try {
+    console.log("Connecting Mongo...");
 
-app.use("/api/auth", authRoutes);
-app.use("/api/workflow", workflowRoutes);
+    await mongoose.connect(process.env.MONGO_URI!);
 
-app.get("/", (req, res) => {
-  res.send("Welcome to TriggerFlow Server!");
-});
+    console.log("✅ Mongo connected");
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("✅ Mongo connected");
-  console.log("🚀 Server running on port", process.env.PORT || 3000);
-});
+    app.use("/api/auth", authRoutes);
+    app.use("/api/workflow", workflowRoutes);
+
+    app.get("/", (req, res) => {
+      res.send("Welcome to TriggerFlow Server!");
+    });
+
+    const PORT = process.env.PORT || 3000;
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on ${PORT}`);
+    });
+
+  } catch (err) {
+    console.error("❌ Mongo connection failed:", err);
+    process.exit(1);
+  }
+}
+
+startServer();
